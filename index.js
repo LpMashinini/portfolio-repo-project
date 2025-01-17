08163c48bac2e65c89e7fa4b98e9cae61a9eb7e0
Matshinini-Front-end-developer <lebogangp84@gmail.com>
2025-01-17 02:50:29 +0200

    added modification

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ const projectLinkIcons = document.querySelectorAll(".fa-up-right-from-square");
 const project_btn = document.querySelector(".projectsBtn");
 const add_projects = document.querySelector("#addProject");
 const texts = document.querySelectorAll(".dark");
-const titles = document.querySelectorAll(".title");
+const title = document.querySelector(".title");
 const circles = document.querySelectorAll(".circle");
 const links = document.querySelectorAll(".link");
 const name = document.querySelector(".name");
 const btn_submit = document.querySelector(".btns");
-const menuBar = document.querySelector(".fa-bars"); 
-const spanLine = document.querySelectorAll(".line"); 
+const menuBar = document.querySelector(".fa-bars");
+const spanLine = document.querySelectorAll(".line");
 
 
 
@@ -44,10 +44,10 @@ links.forEach(link => {
 const checkbox = document.querySelector('.input');
 const downloadLink = document.getElementById('cvDownloadLink');
 
-checkbox.addEventListener('change', function() {
-    if (checkbox.checked) {
-        downloadLink.click();
-    }
+checkbox.addEventListener('change', function () {
+  if (checkbox.checked) {
+    downloadLink.click();
+  }
 });
 
 // Dark Mode Script
@@ -62,9 +62,9 @@ const toggleMode = () => {
   icon.classList.toggle("fa-sun", isDarkMode);
   icon.classList.toggle("fa-moon", !isDarkMode);
 
-  if(isDarkMode){
+  if (isDarkMode) {
     darkMode();
-  }else{
+  } else {
     lightMode();
   }
 }
@@ -76,19 +76,20 @@ icon.addEventListener('click', toggleMode)
 function darkMode() {
 
   iconContainers.forEach(iconContainer => iconContainer.style.background = "rgb(37, 43, 105)");
-  Icons.forEach(Icon =>  Icon.classList.add("active"));
+  Icons.forEach(Icon => Icon.classList.add("active"));
   labels.forEach(label => label.classList.add("active-label"));
   iconSvgs.forEach(iconSvg => iconSvg.classList.add("activesvg"));
-  btns.forEach( btn =>   btn.classList.add("active-btn"));
-  inputIcons.forEach(inputIcon =>   inputIcon.classList.add("active-inputIcon"));
-  projects.forEach(project =>   project.classList.add("active-project"));
-  projectLinks.forEach(projectLink =>   projectLink.classList.add("active-Link"));
-  projectLinkIcons.forEach( projectLinkIcon =>  projectLinkIcon.classList.add("activeLinkIcon"));
-  nav_container.forEach( navCon => navCon.classList.add("active-nav-bag"));
-  texts.forEach( text => text.classList.add("active-light"));
-  circles.forEach( circle => circle.classList.add("active-circle"));
-  links.forEach( link => link.classList.add("link-circle"));
-  spanLine.forEach( line => line.classList.add("active-line"));
+  btns.forEach(btn => btn.classList.add("active-btn"));
+  inputIcons.forEach(inputIcon => inputIcon.classList.add("active-inputIcon"));
+  projects.forEach(project => project.classList.add("active-project"));
+  projectLinks.forEach(projectLink => projectLink.classList.add("active-Link"));
+  projectLinkIcons.forEach(projectLinkIcon => projectLinkIcon.classList.add("activeLinkIcon"));
+  nav_container.forEach(navCon => navCon.classList.add("active-nav-bag"));
+  texts.forEach(text => text.classList.add("active-light"));
+  circles.forEach(circle => circle.classList.add("active-circle"));
+  links.forEach(link => link.classList.add("link-circle"));
+  spanLine.forEach(line => line.classList.add("active-line"));
+  title.classList.add('active-title');
   nav_el.classList.add("active-nav-el");
   name.classList.add("active-name");
   btn_submit.classList.add("active-submit");
@@ -97,20 +98,21 @@ function darkMode() {
 function lightMode() {
 
   iconContainers.forEach(iconContainer => iconContainer.style.background = "hsl(0, 0%, 91%)");
-  Icons.forEach(Icon =>  Icon.classList.remove("active"));
+  Icons.forEach(Icon => Icon.classList.remove("active"));
   labels.forEach(label => label.classList.remove("active-label"));
   iconSvgs.forEach(iconSvg => iconSvg.classList.remove("activesvg"));
-  btns.forEach( btn =>   btn.classList.remove("active-btn"));
-  inputIcons.forEach(inputIcon =>   inputIcon.classList.remove("active-inputIcon"));
-  projects.forEach(project =>   project.classList.remove("active-project"));
-  projectLinks.forEach(projectLink =>   projectLink.classList.remove("active-Link"));
-  projectLinkIcons.forEach( projectLinkIcon =>  projectLinkIcon.classList.remove("activeLinkIcon"));
-  nav_container.forEach( navCon => navCon.classList.remove("active-nav-bag"));
-  texts.forEach( text => text.classList.remove("active-light"));
-  circles.forEach( circle => circle.classList.remove("active-circle"));
-  links.forEach( link => link.classList.remove("link-circle"));
-  spanLine.forEach( line => line.classList.remove("active-line"));
+  btns.forEach(btn => btn.classList.remove("active-btn"));
+  inputIcons.forEach(inputIcon => inputIcon.classList.remove("active-inputIcon"));
+  projects.forEach(project => project.classList.remove("active-project"));
+  projectLinks.forEach(projectLink => projectLink.classList.remove("active-Link"));
+  projectLinkIcons.forEach(projectLinkIcon => projectLinkIcon.classList.remove("activeLinkIcon"));
+  nav_container.forEach(navCon => navCon.classList.remove("active-nav-bag"));
+  texts.forEach(text => text.classList.remove("active-light"));
+  circles.forEach(circle => circle.classList.remove("active-circle"));
+  links.forEach(link => link.classList.remove("link-circle"));
+  spanLine.forEach(line => line.classList.remove("active-line"));
   nav_el.classList.remove("active-nav-el");
+  title.classList.remove('active-title');
   name.classList.remove("active-name");
   btn_submit.classList.remove("active-submit");
 
@@ -139,7 +141,7 @@ ScrollReveal().reveal(".icons", {
   delay: 300,
   origin: "bottom",
 });
-ScrollReveal().reveal(".label", {
+ScrollReveal().reveal(".socials", {
   delay: 300,
   origin: "left",
 });
@@ -192,3 +194,41 @@ ScrollReveal().reveal(".right", {
   delay: 300,
   origin: "left",
 });
+
+
+// send mail script
+
+function sendMail() {
+
+  if(document.querySelector("#name").value === '' || document.querySelector(".message").value === ''){
+    return alert("fill up all forms");
+  }
+
+  var params = {
+    name: document.querySelector("#name").value,
+    email: document.querySelector(".email").value,
+    message: document.querySelector(".message").value,
+
+  }
+
+  const serviceID = "service_euz0sjd";
+  const templateID = "template_cxelpss";
+
+  emailjs.send(serviceID, templateID,params)
+    .then(
+      res => {
+
+        document.querySelector("#name").value = '';
+        document.querySelector(".email").value = '';
+        document.querySelector(".message").value = '';
+
+        console.log(res);
+
+        alert("Message Send Successfully");
+      }
+    )
+    .catch((err) => console.log(err)
+    );
+
+
+}
